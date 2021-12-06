c23dabf0f9111dc09ab4c673d423226a56077007
h-emma <emmah92@hotmail.com>
2021-12-06 08:55:21 +0100

    Test

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-// const words = [
-//   'God Jul',
-//   'Merry Christmas',
-//   'Sretan Božić',
-//   'Glædelig Jul',
-//   'Vrolijk Kerstfeest',
-//   'Feliz Natal',
-//   'Craciun Fericit',
-//   'Nadolig Llawen',
-//   'Boldog Karácsonyt',
-//   'Frohe Weihnachten',
-//   'Veselé Vánoce',
-//   'Feliz Navidad',
-//   'срећан Божић',
-//   'счастливого Рождества',
-// ];
+const wordsArray = [
+  'God Jul',
+  'Merry Christmas',
+  'Sretan Božić',
+  'Glædelig Jul',
+  'Vrolijk Kerstfeest',
+  'Feliz Natal',
+  'Craciun Fericit',
+  'Nadolig Llawen',
+  'Boldog Karácsonyt',
+  'Frohe Weihnachten',
+  'Veselé Vánoce',
+  'Feliz Navidad',
+  'срећан Божић',
+  'счастливого Рождества',
+];
+
+const words = document.querySelector('h1');
+window.onload= () =>
+generateRandomWord(wordsArray);
+ addEventListener('keydown', () => generateRandomWord(wordsArray));
+function (){
+  const word = words[Math.floor(Math.random() * words.length)];
+}
 
 // const words = document.querySelectorAll('h1');
 // words.forEach((word) => {
-//   word.addEventListener('keydown', () => {
-//     window.alert(`Yes, you are ${qouteSpan.textContent}`);
-//   });
+//   word.addEventListener('keydown', () => {});
+
 // });
 
+// Function that
 const chrismasSong = document.getElementById('chrismasSong');
 document.getElementById('icon').addEventListener('click', function () {
   if (chrismasSong.paused) {
@@ -31,9 +39,4 @@ document.getElementById('icon').addEventListener('click', function () {
     chrismasSong.pause();
     icon.src = '/image/play.svg';
   }
-});
-
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  window.alert('You are doing amazing, sweetie!');
 });
