38779888c0dedb7828f632d5becf5253b94a739c
h-emma <emmah92@hotmail.com>
2021-12-07 11:49:12 +0100

    Remove code that is not in use, add comment, add a classList and an loop

--- index.js
+++ index.js
@@ -14,29 +14,36 @@ const words = [
   'срећан Божић',
   'счастливого Рождества',
 ];
-
+// Make all the words in the array to upper case
 const formattedWords = words.map((item) => {
   return item.toUpperCase();
 });
+// Function change words when mouse is over the text
+const h1 = document.querySelector('h1');
+h1.addEventListener('mouseover', function () {
+  const word = words[Math.floor(Math.random() * words.length)];
+  h1.textContent = word;
+});
+// Function change words when the text is touched
+h1.addEventListener('touchend', function () {
+  const word = words[Math.floor(Math.random() * words.length)];
+  h1.textContent = word;
+});
 
-document.querySelector('h1').innerHTML = words;
-// document.getElementById('randomXmans').addEventListener('click', function () {
-//   const word = words[Math.floor(Math.random() * words.length)];
-// });
-
-// Function changes color on h1 depending if song is palyed or paused
+// Function make the chrismas tree dance when the mucis is palyed
 const chrismasSong = document.getElementById('chrismasSong');
 chrismasSong.addEventListener('play', function () {
-  document.querySelector('h1').style.color = `#115E0A`;
+  document.querySelector('.emoji').classList.add('dance');
 });
-
+// Function make the chrismas tree stop dance when the mucis is paused
 chrismasSong.addEventListener('pause', function () {
-  document.querySelector('h1').style.color = `#000000`;
+  document.querySelector('.emoji').classList.remove('dance');
 });
 
-// Function that play and paused song and changed image depending if the song is palyed or paused
+// Function that play and paused song and changed image depending if the song is palyed or paused, the song is on a loop
 document.getElementById('icon').addEventListener('click', function () {
   if (chrismasSong.paused) {
+    chrismasSong.loop = true;
     chrismasSong.play();
     icon.src = '/image/pause.svg';
   } else {
